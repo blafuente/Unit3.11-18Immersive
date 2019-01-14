5dbbe77fd583962c5258b10611850dbef1cdbbd8
Brian Lafuente <loulafuente@MacBook-Pro-5.local>
2019-01-14 13:49:52 -0500

    callback

---
+++ node101/callback.js
@@ -0,0 +1,11 @@
+// a callback is a function for some other function to run. 
+// a function to be called after by another function
+
+// In JaveScript, functions are first-class objects.
+// That means... you can do almost anything with
+// a function that you can do WITH any object.
+// - pass them around 
+// - assign it to a variable 
+// - overwrite it
+function x() {}
+console.log(typeof(x))
