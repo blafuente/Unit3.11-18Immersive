da5705045463137309d7de2f7556a33aac66fc83
Brian Lafuente <loulafuente@MacBook-Pro-5.local>
2019-01-14 10:38:45 -0500

    node101 init commit

---
+++ node101/firstFile.js
@@ -0,0 +1,36 @@
+const x = 1;
+console.log(x);
+// console.log(window);
+// window.alert("I'm in the browser");
+// document.querySelector('body');
+
+let atlTeams = [
+    'Braves',
+    'Falcons',
+    'Hawks',
+    'United'
+]
+
+atlTeams = {
+    football: 'Falcons',
+    basketball: 'Braves',
+    baseball: 'Braves',
+    soccer: 'United'
+}
+
+
+// console.log(atlTeams, football);
+
+// like python, you can 
+// import DateTime
+// pip install pygame
+// import pygame
+
+const os = require('os');
+// console.log(os);
+const cpus = os.cpus();
+console.log(cpus);
+const freeMem = os.freemem();
+console.log(freeMem);
+const nI = os.networkInterfaces();
+console.log(nI)
