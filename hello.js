6e5d29e8bdc896c087c01ec4fc9a4b85240aa03f
Brian Lafuente <loulafuente@MacBook-Pro-5.local>
2019-01-14 10:39:28 -0500

    hello self-exercise

---
+++ hello.js
@@ -0,0 +1,31 @@
+// var http = require('http');
+
+// http.createServer(function (request, respones){
+//     response.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     }); 
+//     response.write('Hello World');
+//     response.end();
+
+// }).listen(3000);
+
+// console.log('Server started');
+
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+response.writeHead(200, {
+
+'Content-Type': 'text/plain'
+
+});
+
+response.write('Hello World');
+
+response.end();
+
+}).listen(3000);
+
+console.log('Server started');
