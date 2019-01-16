7a27bad7782dcacf2aa90ff69aee41de9c89e642
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-01-16 10:28:42 -0500

    jan 16

--- node101/nodejs Exercises/fileO.js
+++ node101/nodejs Exercises/fileO.js
@@ -14,3 +14,33 @@ const contents = fs.readFileSync('crop.js', 'utf-8');
 console.log(contents);
 const contents2 = fs.readFileSync('nodejsexercise.js', 'utf-8');
 console.log(contents2);
+
+fs.readFile('dronedata.txt',(err,contents)=>{
+    // get location from data
+    // check for objects that were moving
+    // loss signal
+    // process precip data, so we can chart last nights rain
+})
+
+
+let fs = {};
+fs.readFile('package.json',(err,contents)=>{
+    // do cool stuff
+})
+
+//Inside of Node!!!
+fs.readFile = function(fileToOpen,calbackToRun){
+    // go open the SSD and find fileToOpen
+    // reads the buffer
+    // set buffer from file = data
+    // if there was an error, set errorMessage
+    callbackToRun(errorMessage, data);
+}
+// Inside of Node!
+
+
+fs.readFile('cropdata.js',(err,contents)=>{
+    // put it on a webpage 
+    // send to Harvard for research
+    // organize by food type for a dieticia
+})
