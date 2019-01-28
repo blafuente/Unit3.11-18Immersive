1ce55d21c52177c1a7d81c4ff6fa994022276716
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-01-28 09:33:48 -0500

    jan 28

--- todoMysql/routes/index.js
+++ todoMysql/routes/index.js
@@ -49,12 +49,60 @@ router.post('/addItem',(req,res,next)=>{
     }else{
       // Query succeeded, Forward user  to homepage
       res.redirect('/');
-      
     }
   })
-  
-  // res.json(req.body)
+})
+// a wildcard route in express
+// has a : in front of it
+router.get('/delete/:id',(req,res)=>{
+  // wildcards routes are available in req.params
+  // res.json(req.params);
+  const deleteQuery = `DELETE FROM tasks WHERE id = ?;`;
+  connection.query(deleteQuery,[req.params.id],(error)=>{
+    res.redirect('/');
+  })
+})
 
+router.get('/edit/:id',(req, res)=>{
+  // this route should be a pre-populated form
+  // with the element with this id values
+  const selectQuery = `SELECT * FROM tasks
+    WHERE id = ?;`;
+  connection.query(selectQuery,[req.params.id],(err,results)=>{
+    let formattedDate = formatDate(results[0].taskDate);
+    results[0].taskDate = formattedDate;
+    
+    res.render('edit',{
+      task: results[0]
+    });
+  })
+})
+
+router.post('/editIteam',(req,res)=>{
+  // Update the DB
+  const updateQuery = `UPDATE tasks SET
+    taskName = ?,
+    taskDate = ? 
+    WHERE id = ?`
+  connection.query(updateQuery,[req.body.newTask, req.body.newTaskDate, req.body.taskId],(error,results)=>{
+    if(error){
+      throw error;
+    }else{
+      res.redirect('/');
+    }
+  })
 })
 
 module.exports = router;
+
+function formatDate(date) {
+  let d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
